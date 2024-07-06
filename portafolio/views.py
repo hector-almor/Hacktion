9a6cae34b1b9a3f0b82b4ba688eaeacb1dbb161a
hector-almor <hectoram2347@gmail.com>
2024-07-06 16:27:38 -0600

    Hello world!

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView): 
+    template_name = "home.html"
